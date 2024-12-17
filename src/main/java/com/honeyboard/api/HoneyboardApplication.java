f8d9e4d8dc332e64124dd3e922e464b0a04b5da0
Yurim <zyu22@naver.com>
2024-12-17 15:06:40 +0900

    Initial commit

---
+++ src/main/java/com/honeyboard/api/HoneyboardApplication.java
@@ -0,0 +1,13 @@
+package com.honeyboard.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneyboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneyboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/honeyboard/api/HoneyboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honeyboard.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
