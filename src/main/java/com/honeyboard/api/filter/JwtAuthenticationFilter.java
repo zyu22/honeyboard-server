5b67bf39f14db114c2a9c41cf1dd224ce2f125e8
JaeseoHan <hanahyun1@naver.com>
2024-12-18 09:14:28 +0900

    [feat] 인증시스템 초기 세팅 - User dto 생성 - EncoderConfig, SecurityConfig 생성 - JwtAuthenticationFilter 생성
    - SecurityConfig에 필요한 부분들 다 안 만들어서 에러나는 상태

---
+++ src/main/java/com/honeyboard/api/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,9 @@
+package com.honeyboard.api.filter;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+	
+	
+
+}
