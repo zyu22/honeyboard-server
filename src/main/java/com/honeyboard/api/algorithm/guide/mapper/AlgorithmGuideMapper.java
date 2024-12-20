da746b2dab3360927a8117a9f07be45769b71990
YuJeong <hao_yj@naver.com>
2024-12-21 02:47:27 +0900

    [Feat] 알고리즘 개념 CRUD API 개발 #7

--- src/main/java/com/honeyboard/api/algorithm/guide/mapper/AlgorithmGuideMapper.java
+++ src/main/java/com/honeyboard/api/algorithm/guide/mapper/AlgorithmGuideMapper.java
@@ -1,5 +1,22 @@
 package com.honeyboard.api.algorithm.guide.mapper;
 
+import com.honeyboard.api.algorithm.guide.model.AlgorithmGuide;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface AlgorithmGuideMapper {
 
+    List<AlgorithmGuide> getAlgorithmGuides(int generationId);
+
+    AlgorithmGuide getAlgorithmGuideDetail (int id, boolean bookmarkflag);
+
+    List<AlgorithmGuide> searchAlgorithmGuide(int generationId, String title);
+
+    int addAlgorithmGuide (int generationId, AlgorithmGuide algorithmGuide);
+
+    int updateAlgorithmGuide(int id, AlgorithmGuide algorithmGuide);
+
+    int deleteAlgorithmGuide(int id);
 }
