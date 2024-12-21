a575ba3a2c046156423466a93d593b4056a491d3
sujin <unikang16@gmail.com>
2024-12-22 01:14:19 +0900

    페이지네이션 따로 처리 필요

--- src/main/java/com/honeyboard/api/algorithm/solution/service/AlgorithmSolutionService.java
+++ src/main/java/com/honeyboard/api/algorithm/solution/service/AlgorithmSolutionService.java
@@ -1,5 +1,14 @@
 package com.honeyboard.api.algorithm.solution.service;
 
+import java.util.List;
+
+import com.honeyboard.api.algorithm.solution.model.AlgorithmSolution;
+
 public interface AlgorithmSolutionService {
 
+	boolean addAlgorithmSolution(int problemId, AlgorithmSolution algorithmSolution);
+	List<AlgorithmSolution> getAllAlgorithmSolution(int problemId, int generationId, List<String> languages, String sortType, int page);
+	AlgorithmSolution getAlgorithmSolution(int solutionId);
+	boolean updateAlgorithmSolution(AlgorithmSolution algorithmSolution);
+	boolean softDeleteAlgorithmSolution(int solutionId);
 }
--- src/main/java/com/honeyboard/api/algorithm/solution/service/AlgorithmSolutionServiceImpl.java
+++ src/main/java/com/honeyboard/api/algorithm/solution/service/AlgorithmSolutionServiceImpl.java
@@ -1,5 +1,47 @@
 package com.honeyboard.api.algorithm.solution.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.honeyboard.api.algorithm.solution.mapper.AlgorithmSolutionMapper;
+import com.honeyboard.api.algorithm.solution.model.AlgorithmSolution;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class AlgorithmSolutionServiceImpl implements AlgorithmSolutionService {
+	
+	private final AlgorithmSolutionMapper algorithmSolutionMapper;
+
+	@Override
+	public boolean addAlgorithmSolution(int problemId, AlgorithmSolution algorithmSolution) {
+		int result = algorithmSolutionMapper.insertAlgorithmSolution(problemId, algorithmSolution);
+		return result == 1;
+	}
+
+	@Override
+	public List<AlgorithmSolution> getAllAlgorithmSolution(int problemId, int generationId, List<String> languages,
+			String sortType, int page) {
+		return algorithmSolutionMapper.selectAllAlgorithmSolution(problemId, generationId, languages, sortType, page);
+	}
+
+	@Override
+	public AlgorithmSolution getAlgorithmSolution(int solutionId) {
+		return algorithmSolutionMapper.selectAlgorithmSolution(solutionId);
+	}
+
+	@Override
+	public boolean updateAlgorithmSolution(AlgorithmSolution algorithmSolution) {
+		int result = algorithmSolutionMapper.updateAlgorithmSolution(algorithmSolution);
+		return result == 1;
+	}
+
+	@Override
+	public boolean softDeleteAlgorithmSolution(int solutionId) {
+		int result = algorithmSolutionMapper.deleteAlgorithmSolution(solutionId);
+		return result == 1;
+	}
 
 }
