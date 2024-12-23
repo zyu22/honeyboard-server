7f875db8d2a5055fdf34c14a73822428baa4fbd7
sujin <unikang16@gmail.com>
2024-12-23 14:10:08 +0900

    serviceImpl 수정

--- src/main/java/com/honeyboard/api/algorithm/solution/service/AlgorithmSolutionServiceImpl.java
+++ src/main/java/com/honeyboard/api/algorithm/solution/service/AlgorithmSolutionServiceImpl.java
@@ -24,7 +24,16 @@ public class AlgorithmSolutionServiceImpl implements AlgorithmSolutionService {
 	@Override
 	public PageResponse<AlgorithmSolution> getAllAlgorithmSolution(int problemId, Integer generationId, List<String> languages,
 			String sortType, int page) {
-		return algorithmSolutionMapper.selectAllAlgorithmSolution(problemId, generationId, languages, sortType, page);
+		// 전체 풀이 수
+	    int totalElements = algorithmSolutionMapper.countAlgorithmSolutions(problemId, generationId, languages);
+	    
+	    // PageInfo(pageSize = 9)
+	    PageInfo pageInfo = new PageInfo(page, 9, totalElements);
+	    int offset = (page - 1) * 9;
+	    
+	    //전체 조회 -> List
+	    List<AlgorithmSolution> solutions = algorithmSolutionMapper.selectAllAlgorithmSolution(problemId, generationId, languages, sortType, offset);
+	    return new PageResponse<>(solutions, pageInfo);
 	}
 
 	@Override
