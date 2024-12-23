41e9b2e52e79fd4d737439d15de6a03b95de04a2
clapsheep <sktndid1203@naver.com>
2024-12-23 22:08:41 +0900

    [setting] Spring 서버 실행을 위한 설정 - mapper.xml 기본 매퍼 설정 - application.properties, dev 각각 용도에 맞게 구성 재배치 및 정리, redis 레거시 설정 변경 - DBConfig MapperScan 경로 수정

--- src/main/java/com/honeyboard/api/config/DBConfig.java
+++ src/main/java/com/honeyboard/api/config/DBConfig.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan(basePackages={"com.honeyboard.api.*.model.mapper"})
+@MapperScan(basePackages = {"com.honeyboard.api.**.mapper"})
 public class DBConfig {
 
 }
