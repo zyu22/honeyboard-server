9a3c1ad47f9f6e13bea73b0596f1556281fb08ab
clapsheep <sktndid1203@naver.com>
2024-12-27 00:43:54 +0900

    [refactor] OAuth2 로그인, 회원가입 기능 관련 수정 - redirect 시켜야 하는 경로 설정 - User 이름만 들어가는 생성자 설정 - OAuth 로그인 시 비밀번호 처리 로직 삭제 - cors 설정 로컬 호스트 5173,3000 등록

--- src/main/java/com/honeyboard/api/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/honeyboard/api/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,60 +1,57 @@
 package com.honeyboard.api.handler;
 
-import java.io.IOException;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.stereotype.Component;
-
 import com.honeyboard.api.jwt.model.service.JwtService;
 import com.honeyboard.api.user.model.CustomUserDetails;
 import com.honeyboard.api.user.model.User;
 import com.honeyboard.api.user.model.service.UserService;
 import com.honeyboard.api.util.CookieUtil;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-	
-	private final JwtService jwtService;
+
+    private final JwtService jwtService;
     private final CookieUtil cookieUtil;
     private final UserService userService;
 
-	@Override
-	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-			Authentication authentication) throws IOException, ServletException {
-		log.debug("OAuth2AuthenticationSuccessHandler/onAuthenticationSuccess");
-		CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal(); // 정보 가져오기
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        log.debug("OAuth2AuthenticationSuccessHandler/onAuthenticationSuccess");
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal(); // 정보 가져오기
         User user = customUserDetails.getUser();
-        
-        if (customUserDetails.getAttributes() != null && 
+
+        if (customUserDetails.getAttributes() != null &&
                 Boolean.TRUE.equals(customUserDetails.getAttributes().get("isNewUser"))) { // 처음 로그인한 유저면
+            //String domain = customUserDetails. 추후에 oauth 제공사 정보를 담아서 아래 redirect domain 들어가는 곳에 넣어야함
             String temporaryToken = jwtService.generateTemporaryToken(user.getEmail()); // 임시토큰 발급
-            cookieUtil.addCookie(response, "temporary_token", temporaryToken, 
+            cookieUtil.addCookie(response, "temporary_token", temporaryToken,
                     (int) (jwtService.getTemporaryTokenExpire() / 1000)); // httponly cookies로 전달
-//            	response.sendRedirect("honeyboard-client-url/signup?token=" + temporaryToken); 회원가입 url 나오면 고치기
-                return;
-            } // http://localhost:8080/oauth2/authorization/google
+            response.sendRedirect("http://localhost:5173/oauth/google/additional");
+            return;
+        } // http://localhost:8080/oauth2/authorization/google
 
         // 여기까지 오면 기존 유저
-            String accessToken = jwtService.generateAccessToken(user); // 토큰 발급
-            String refreshToken = jwtService.generateRefreshToken(user);
-            
-            cookieUtil.addCookie(response, "access_token", accessToken, 
+        String accessToken = jwtService.generateAccessToken(user); // 토큰 발급
+        String refreshToken = jwtService.generateRefreshToken(user);
+
+        cookieUtil.addCookie(response, "access_token", accessToken,
                 (int) (jwtService.getAccessTokenExpire() / 1000)); // httponly cookies에 담기
-            cookieUtil.addCookie(response, "refresh_token", refreshToken, 
+        cookieUtil.addCookie(response, "refresh_token", refreshToken,
                 (int) (jwtService.getRefreshTokenExpire() / 1000));
-                
+
 //            response.sendRedirect("honeyboard-client-url");
-        }
+    }
 
 }
--- src/main/java/com/honeyboard/api/user/model/User.java
+++ src/main/java/com/honeyboard/api/user/model/User.java
@@ -1,13 +1,8 @@
 package com.honeyboard.api.user.model;
 
-import java.sql.Date;
+import lombok.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import java.sql.Date;
 
 @Getter
 @Setter
@@ -17,14 +12,17 @@ import lombok.ToString;
 @Builder
 public class User {
 
-	private int userId;
-	private String email;
-	private String password;
-	private String name;
-	private int generationId;
-	private String role;
-	private String loginType;
-	private boolean isSsafy;
-	private Date createdAt;
-	
+    private int userId;
+    private String email;
+    private String password;
+    private String name;
+    private int generationId;
+    private String role;
+    private String loginType;
+    private boolean isSsafy;
+    private Date createdAt;
+
+    public User(String name) {
+        this.name = name;
+    }
 }
