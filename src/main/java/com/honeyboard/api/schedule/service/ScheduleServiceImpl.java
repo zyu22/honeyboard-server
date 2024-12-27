7386f2f71407c311f8fbb8e93317780c07d80492
sujin <unikang16@gmail.com>
2024-12-28 02:11:59 +0900

    스케줄 조회 수정

--- src/main/java/com/honeyboard/api/schedule/service/ScheduleServiceImpl.java
+++ src/main/java/com/honeyboard/api/schedule/service/ScheduleServiceImpl.java
@@ -1,14 +1,20 @@
 package com.honeyboard.api.schedule.service;
 
-import com.honeyboard.api.schedule.model.Schedule;
+import com.honeyboard.api.project.track.model.TrackProject;
 import com.honeyboard.api.schedule.mapper.ScheduleMapper;
+import com.honeyboard.api.schedule.model.Schedule;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ScheduleServiceImpl implements ScheduleService {
 
 	private final ScheduleMapper scheduleMapper;
@@ -21,7 +27,36 @@ public class ScheduleServiceImpl implements ScheduleService {
 
 	@Override
 	public List<Schedule> getScheduleByMonth(int year, int month, Integer generationId, String role) {
-		return scheduleMapper.selectScheduleByMonth(year, month, generationId, role);
+		List<Schedule> schedules = scheduleMapper.selectScheduleByMonth(year, month, generationId, role);
+		List<TrackProject> trackProjects = scheduleMapper.selectTrackProjectByMonth(year, month, generationId);
+
+		//날짜 포맷 변경
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+		for (TrackProject project : trackProjects) {
+			try {
+				Schedule schedule = new Schedule();
+				schedule.setScheduleId(project.getTrackProjectId());
+				schedule.setTitle(project.getTitle());
+
+				java.util.Date utilDate = formatter.parse(project.getCreatedAt());
+				java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+
+				schedule.setScheduleType("TRACK");
+				schedule.setPublicAccess(true);
+				schedule.setGenerationId(project.getGenerationId());
+				schedule.setUserId(project.getUserId());
+
+				schedules.add(schedule);
+			} catch (ParseException e) {
+				log.error("날짜 변환 중 오류 발생: ", e);
+			}
+		}
+		//날짜순 정렬
+		Collections.sort(schedules,
+				(s1, s2) -> s1.getStartDate().compareTo(s2.getStartDate()));
+
+		return schedules;
 	}
 
 	@Override
