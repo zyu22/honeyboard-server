74d5a61b8dc2ed3133a6cdf19f8cdf409e6ade8d
SungMoonPark <taim831@naver.com>
2024-12-28 23:40:43 +0900

    [refactor] 관통 게시글 CRUD API 리팩토링 - DTO에 유저 이름을 담을 수 있는 teamMembers 추가

--- src/main/java/com/honeyboard/api/project/track/model/TrackProjectBoard.java
+++ src/main/java/com/honeyboard/api/project/track/model/TrackProjectBoard.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -20,4 +22,5 @@ public class TrackProjectBoard {
     private String updatedAt;
     private boolean isDeleted;
 
+    private String teamMembers;
 }
