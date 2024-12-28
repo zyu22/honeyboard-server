39f7d5a7270505804e50c4b3de6e82ab8d18bd1e
JaeSeoHan <hanahyun1@gmail.com>
2024-12-28 20:40:42 +0900

    [feat] Custom Annotation 설정 - @CurrentUser 어노테이션 설정 - HttpOnly Cookies에 있는 access token에 담긴 유저 정보 얻어오기 위해 CurrentUserArgumentResolver 구현 및 Configuration

--- src/main/java/com/honeyboard/api/common/response/FinaleProjectResponse.java
+++ src/main/java/com/honeyboard/api/common/response/FinaleProjectResponse.java
@@ -1,5 +1,7 @@
-package com.honeyboard.api.project.finale.model;
+package com.honeyboard.api.common.response;
 
+import com.honeyboard.api.project.finale.model.FinaleProject;
+import com.honeyboard.api.project.finale.model.FinaleTeam;
 import com.honeyboard.api.user.model.User;
 import lombok.*;
 
