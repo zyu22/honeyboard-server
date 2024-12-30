f93c7ae8d4e10cd2a06dfd1f4f551dc63169fcbc
kkangssu <unikang16@gmail.com>
2024-12-31 01:11:22 +0900

    Update Schedule.java
    LocalDate 수정

--- src/main/java/com/honeyboard/api/schedule/model/Schedule.java
+++ src/main/java/com/honeyboard/api/schedule/model/Schedule.java
@@ -1,8 +1,7 @@
 package com.honeyboard.api.schedule.model;
 
 import lombok.*;
-
-import java.sql.Date;
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -14,8 +13,8 @@ public class Schedule {
 
 	private int scheduleId;
 	private String content;
-	private Date startDate;
-	private Date endDate;
+	private LocalDate startDate;
+	private LocalDate endDate;
 	private String scheduleType;
 	private boolean publicAccess;
 	private int userId;
