e87957ab13c8c1117531981b945261a6c6da43b4
JaeSeoHan <hanahyun1@gmail.com>
2024-12-31 15:53:51 +0900

    [refactor] AuthController 및 의존성 주입된 서비스단 에러핸들링 방식으로 수정 - 6개의 새로운 에러 - AuthController, userService, jwtService, emailService, verificationService 에러핸들링 방식으로 수정

---
+++ src/main/java/com/honeyboard/api/exception/VerificationCodeException.java
@@ -0,0 +1,9 @@
+package com.honeyboard.api.exception;
+
+public
+class VerificationCodeException extends RuntimeException {
+    public
+    VerificationCodeException(String message) {
+        super(message);
+    }
+}
