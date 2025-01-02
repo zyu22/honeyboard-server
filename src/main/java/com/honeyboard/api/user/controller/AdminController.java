83b69c5064e3cb83ba2bdd27528952f6c6a3154d
Yurim <zyu22@naver.com>
2025-01-02 13:09:41 +0900

    [feat] 기수 전체 조회 API 개발

--- src/main/java/com/honeyboard/api/user/controller/AdminController.java
+++ src/main/java/com/honeyboard/api/user/controller/AdminController.java
@@ -1,4 +1,14 @@
 package com.honeyboard.api.user.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
+@Slf4j
 public class AdminController {
+    private final AdminService adminService;
 }
