39c0b8834f126f2b0fa8d4583e59ebe4ecf0f123
Yurim <zyu22@naver.com>
2025-01-02 13:38:13 +0900

    [refactor] 웹 개념 : 썸네일 추가로 API 수정

--- src/main/java/com/honeyboard/api/user/service/AdminServiceimpl.java
+++ src/main/java/com/honeyboard/api/user/service/AdminServiceimpl.java
@@ -1,7 +1,14 @@
 package com.honeyboard.api.user.service;
 
+import com.honeyboard.api.user.model.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AdminServiceimpl implements AdminService {
+    @Override
+    public List<User> getUserByGeneration(int generationId) {
+        return List.of();
+    }
 }
