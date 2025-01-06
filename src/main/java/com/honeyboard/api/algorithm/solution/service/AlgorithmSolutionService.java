89b5f5b96cf949cab1f9fdf2b4abdbe8304cbe61
sujin <unikang16@gmail.com>
2025-01-07 00:28:48 +0900

    [Refactor] generationId 타입Integer -> int

--- src/main/java/com/honeyboard/api/algorithm/solution/service/AlgorithmSolutionService.java
+++ src/main/java/com/honeyboard/api/algorithm/solution/service/AlgorithmSolutionService.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface AlgorithmSolutionService {
 
 	void addAlgorithmSolution(AlgorithmSolution algorithmSolution);
-	PageResponse<AlgorithmSolutionResponse> getAllAlgorithmSolution(int problemId, Integer generationId, List<String> languages, String sortType, int currentPage, int userId);
+	PageResponse<AlgorithmSolutionResponse> getAllAlgorithmSolution(int problemId, int generationId, List<String> languages, String sortType, int currentPage, int userId);
 	AlgorithmSolutionResponse getAlgorithmSolution(int solutionId);
 	void updateAlgorithmSolution(AlgorithmSolution algorithmSolution);
 	void softDeleteAlgorithmSolution(int solutionId);
