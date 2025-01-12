b1815731998edfdf690518408b7532a65360904b
sujin <unikang16@gmail.com>
2025-01-12 18:29:20 +0900

    [Refactor] AlgorithmSolution service, controller 주석처리

--- src/main/java/com/honeyboard/api/algorithm/problem/mapper/AlgorithmProblemMapper.java
+++ src/main/java/com/honeyboard/api/algorithm/problem/mapper/AlgorithmProblemMapper.java
@@ -1,6 +1,10 @@
 package com.honeyboard.api.algorithm.problem.mapper;
 
-import com.honeyboard.api.algorithm.problem.model.AlgorithmProblem;
+import com.honeyboard.api.algorithm.problem.model.request.AlgorithmProblemRequest;
+import com.honeyboard.api.algorithm.problem.model.response.AlgorithmProblemDetail;
+import com.honeyboard.api.algorithm.problem.model.response.AlgorithmProblemList;
+import com.honeyboard.api.algorithm.solution.model.response.AlgorithmSolutionList;
+import com.honeyboard.api.algorithm.tag.model.response.TagResponse;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -8,36 +12,38 @@ import java.util.List;
 
 @Mapper
 public interface AlgorithmProblemMapper {
-    // 조회 관련
-    List<AlgorithmProblem> selectAllAlgorithmProblem(@Param("offset") int offset, @Param("pageSize") int pageSize);
 
-    List<AlgorithmProblem> selectSearchAlgorithmProblem(@Param("offset") int offset, @Param("pageSize") int pageSize,
-                                                        @Param("searchType") String searchType, @Param("keyword") String keyword);
+    // AlgorithmProblem 전체조회
+    List<AlgorithmProblemList> selectAllAlgorithmProblem(@Param("offset") int offset,
+                                                         @Param("pageSize") int pageSize,
+                                                         @Param("searchType") String searchType,
+                                                         @Param("keyword") String keyword);
+    // AlgorithmProblem 전체조회 카운트
+    int countAlgorithmProblem(@Param("searchType") String searchType,
+                              @Param("keyword") String keyword);
 
-    AlgorithmProblem selectAlgorithmProblem(@Param("problemId") int problemId);
+    // AlgorithmProblem 상세조회
+    AlgorithmProblemDetail selectProblemBasicInfo(@Param("problemId") int problemId);
+    List<AlgorithmSolutionList> selectProblemSolutions(@Param("problemId") int problemId);
+    List<TagResponse> selectProblemTags(@Param("problemId") int problemId);
 
-    // 문제 CUD
-    int insertAlgorithmProblem(AlgorithmProblem algorithmProblem);
+    // AlgorithmProblem 작성
+    int insertAlgorithmProblem(@Param("request") AlgorithmProblemRequest request,
+                               @Param("userId") int userId);
+    int existsByUrl(@Param("request") AlgorithmProblemRequest request);
 
-    int updateAlgorithmProblem(@Param("problemId") int problemId, @Param("algorithmProblem") AlgorithmProblem algorithmProblem);
+    // AlgorithmProblem 수정
+    int updateAlgorithmProblem(@Param("request") AlgorithmProblemRequest request,
+                                @Param("problemId") int problemId);
 
+    // AlgorithmProblem 삭제
     int deleteAlgorithmProblem(@Param("problemId") int problemId);
 
     // 태그 관계 관리
-    int insertAlgorithmProblemTags(@Param("problemId") int problemId, @Param("tagIds") List<Integer> tagIds);
-    
-
-    // 존재 여부 확인
-    int existsByUrl(@Param("url") String url);
-
-    int existsById(@Param("id") int id);
-
-    // 카운트
-    int countAlgorithmProblem();
-
-    int countSearchAlgorithmProblem(@Param("searchType") String searchType, @Param("keyword") String keyword);
-
+    int insertAlgorithmProblemTags(@Param("problemId") int problemId,
+                                   @Param("tagIds") List<Integer> tagIds);
     List<Integer> getTagIdsByProblemId(@Param("problemId") int problemId);
+    int deleteSpecificAlgorithmProblemTags(@Param("problemId") int problemId,
+                                           @Param("tagIds") List<Integer> tagIds);
 
-    int deleteSpecificAlgorithmProblemTags(@Param("problemId") int problemId, @Param("tagIds") List<Integer> tagIds);
 }
--- src/main/java/com/honeyboard/api/algorithm/problem/service/AlgorithmProblemService.java
+++ src/main/java/com/honeyboard/api/algorithm/problem/service/AlgorithmProblemService.java
@@ -1,24 +1,21 @@
 package com.honeyboard.api.algorithm.problem.service;
 
-import com.honeyboard.api.algorithm.problem.model.AlgorithmProblem;
-import com.honeyboard.api.common.response.PageResponse;
-
 public interface AlgorithmProblemService {
-    PageResponse<AlgorithmProblem> getAllProblem(int currentPage, int pageSize);
-
-    PageResponse<AlgorithmProblem> searchProblem(int currentPage, int pageSize, String searchType, String keyword);
-
-    AlgorithmProblem getProblem(int problemId);
-
-    AlgorithmProblem addProblem(AlgorithmProblem algorithmProblem);
-
-    AlgorithmProblem updateProblem(int problemId, AlgorithmProblem algorithmProblem);
-
-    boolean softDeleteProblem(int problemId);
-
-    boolean existsByUrl(String url);
-
-    boolean existsById(int id);
+//    PageResponse<AlgorithmProblemList> getAllProblem(int currentPage, int pageSize);
+//
+//    PageResponse<AlgorithmProblem> searchProblem(int currentPage, int pageSize, String searchType, String keyword);
+//
+//    AlgorithmProblem getProblem(int problemId);
+//
+//    AlgorithmProblem addProblem(AlgorithmProblem algorithmProblem);
+//
+//    AlgorithmProblem updateProblem(int problemId, AlgorithmProblem algorithmProblem);
+//
+//    boolean softDeleteProblem(int problemId);
+//
+//    boolean existsByUrl(String url);
+//
+//    boolean existsById(int id);
 
 
 }
