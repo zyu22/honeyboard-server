5324edb97a20f78ff6fec4ca4d7fa5b00c602e50
elitezer0 <diana.yy.jeong@gmail.com>
2025-04-22 01:09:20 +0900

    [refactor] 기수 Id로 팀이 없는 유저 조회, 팀 리스트 조회 시 기수 필터 추가

--- src/main/java/com/honeyboard/api/project/finale/mapper/FinaleTeamMapper.java
+++ src/main/java/com/honeyboard/api/project/finale/mapper/FinaleTeamMapper.java
@@ -14,11 +14,14 @@ public interface FinaleTeamMapper {
     // 팀이 없는 유저 조회
     List<ProjectUserInfo> selectNoFinaleTeamUsers(int finaleProjectId);
 
+    // 기수 ID로 팀이 없는 유저 조회
+    List<ProjectUserInfo> selectNoFinaleTeamUsersByGeneration(int generationId);
+
     // 팀 없는 사람
     List<ProjectUserInfo> selectNoTeamFinaleTeamUsers();
 
     // 팀 리스트 조회
-    List<FinaleTeamList> selectFinaleTeamList();
+    List<FinaleTeamList> selectFinaleTeamList(int generationId);
 
     // 프로젝트 상세 팀 인원 조회
     List<TeamMemberInfo> selectFinaleProjectDetailMembers(int finaleTeamId);
@@ -26,15 +29,17 @@ public interface FinaleTeamMapper {
     int selectFinaleTeamId(int finaleProjectId);
 
     int updateTeamLeader(TeamRequest request);
+
     int deleteTeamMembers(TeamRequest request);
+
     int insertTeamMember(TeamRequest request);
 
     boolean checkTeamMember(@Param("teamId") int teamId,
-                            @Param("userId") int userId);
+            @Param("userId") int userId);
 
     boolean checkBoardTeamMember(@Param("finaleProjectId") int finaleProjectId,
-                                 @Param("finaleProjectBoardId") int finaleProjectBoardId,
-                                 @Param("userId") int userId);
+            @Param("finaleProjectBoardId") int finaleProjectBoardId,
+            @Param("userId") int userId);
 
     int deleteTeam(int finaleTeamId);
 }
